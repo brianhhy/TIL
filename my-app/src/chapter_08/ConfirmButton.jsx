27b763f347bfac9aad864a068cd55222b97935ee
brianhhy <brianhhy68@gmail.com>
2024-04-03 20:33:05 +0900

    useState

--- my-app/src/chapter_08/ConfirmButton.jsx
+++ my-app/src/chapter_08/ConfirmButton.jsx
@@ -4,20 +4,20 @@ class ConfirmButton extends React.Component{
     constructor(props){
         super(props);
 
-        this.state={
-            isConfirmed:false,
+        this.state = {
+            isConfirmed :false,
         };
         
-        this.handleConfirm=this.handleConfirm.bind(this);
+
     }
-    handleConfirm(){
+    handleConfirm = () =>{
         this.setState((prevState)=>({
             isConfirmed: !prevState.isConfirmed,
         }));
     }
     render(){
         return(
-            <button onClick={this.handleConfirmed} disabled={this.state.isConfirmed}>
+            <button onClick={this.handleConfirm} disabled={this.state.isConfirmed}>
                 {this.state.isConfirmed ? "확인됨" : "확인하기"}
             </button>
         );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,10 +15,15 @@
   import Clock from './chapter_04/Clock'
   import CommentList from './chapter_05/CommentList';
   import NotificationList from './chapter_06/NotificationList'
+  import Accommodate from './chapter_07/Accommodate';
+  import ConfirmButton from './chapter_08/ConfirmButton';
+  import LandingPage from './chapter_09/LandingPage';
+  import AttendanceBook from './chapter_10/AttendanceBook'
+  import SignUp from './chapter_11/SignUp';
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
-      <NotificationList />
+      <SignUp />
     </React.StrictMode>,
     document.getElementById('root')
   );
