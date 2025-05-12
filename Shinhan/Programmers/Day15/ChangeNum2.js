4dc74af72423036fdd0850e5600d62e487c90387
brianhhy <brianhhy68@gmail.com>
2025-05-12 12:28:59 +0900

    programmers day 15

---
+++ Shinhan/Programmers/Day15/ChangeNum2.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    var answer = [];
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i] < 50 && arr[i] % 2 == 1){
+            arr[i] *= 2;
+        } else if(arr[i] >= 50 && arr[i] % 2 == 0){
+            arr[i] /= 2;
+        }
+    }
+    return arr;
+}
---
+++ Shinhan/Programmers/Day15/FindString.js
@@ -0,0 +1,4 @@
+function solution(myString, pat) {
+    
+    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
+}
