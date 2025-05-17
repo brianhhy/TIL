a7da4e1599ea7f32a468e70b388ce43768b43648
brianhhy <brianhhy68@gmail.com>
2025-05-17 10:44:04 +0900

    jquery&jsp

---
+++ ModernJS/ch4/var.js
@@ -0,0 +1,16 @@
+var userId = 1;
+var userName = 'lee';
+
+var user = {id : 1, name : 'lee'};
+
+console.log(user.name);
+
+var users = [
+    {id : 1, name : 'Lee'},
+    {id : 2, name : 'Hwang'}    
+]
+
+// 변수의 실행 시점과 호이스팅
+console.log(score);
+var score;
+
--- hipQ/app/quotes/components/RightContent.js
+++
@@ -1,9 +0,0 @@
-import RightQuotemaker from "./RightQuotemaker";
-export default function RightContent() {
-    return (
-      <div className="bg-white w-full max-w-[90vw] h-full max-h-[90vh] flex justify-center items-center">
-        <RightQuotemaker />
-      </div>
-    );
-}
-  
