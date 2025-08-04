9781eaaa1aed39c94ff3cba4720458335ada1238
brianhhy <brianhhy68@gmail.com>
2025-08-04 09:35:06 +0900

    색 수정

--- nextpractice/src/app/layout.js
+++ nextpractice/src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
   title: "고양이 정보 앱",
   description: "고양이에 대한 재미있는 정보를 제공하는 PWA 앱",
   manifest: "/manifest.json",
-  themeColor: "#0070f3",
+  themeColor: "#ffffff",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
   appleWebApp: {
     capable: true,
@@ -49,15 +49,15 @@ export default function RootLayout({ children }) {
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#0070f3" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#0070f3" />
+        <meta name="theme-color" content="#ffffff" />
 
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icon-192x192.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/icon-192x192.png" color="#0070f3" />
+        <link rel="mask-icon" href="/icon-192x192.png" color="#ffffff" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
