d38bc4cd76f7a33f24820bc6a5808790a73fd6fd
brianhhy <brianhhy68@gmail.com>
2025-08-04 10:16:43 +0900

    배경 이미지 추가

--- nextpractice/src/app/page.js
+++ nextpractice/src/app/page.js
@@ -1,17 +1,60 @@
-import styles from "./page.module.css";
+'use client';
+
+import styled from 'styled-components';
 import Button from "./cat/components/Button";
 // import InstallPWA from "./utils/InstallPWA";
 
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BackgroundImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('/assets/HomeBackground.jpeg');
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  z-index: -1;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  z-index: 1;
+`;
+
+const Title = styled.h1`
+  color: white;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+  margin-bottom: 20px;
+  font-size: 2.5rem;
+  font-weight: bold;
+`;
+
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>홈 화면</h1>
+    <Container>
+      <BackgroundImage />
+      <Content>
+        <Title>홈 화면</Title>
         <Button href="/cat" variant="primary">
-          메뉴로 이동
+          알쓸신고 보러가기
         </Button>
-      </main>
+      </Content>
       {/* <InstallPWA /> */}
-    </div>
+    </Container>
   );
 }
