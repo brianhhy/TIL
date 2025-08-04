1279a92adeaeb8b9063a43d5820f2122ba3d0eb8
brianhhy <brianhhy68@gmail.com>
2025-08-04 09:02:16 +0900

    next.config 수정

--- nextpractice/next.config.mjs
+++ nextpractice/next.config.mjs
@@ -2,13 +2,11 @@ import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-    buildExcludes: [/middleware-manifest\.json$/],
-  },
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest\.json$/],
 });
 
 export default nextConfig;
